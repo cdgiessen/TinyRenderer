4d3eb90ca32c90d598897829ebb340b00f96db4e
Charles Giessen <cdgiessen@gmail.com>
2020-11-04 10:11:21 -0700

    Lesson 0, 1, and first half of 2

---
+++ lesson-1/main.cpp
@@ -0,0 +1,118 @@
+#include "tgaimage.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+
+void line_attempt_1(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+{
+    for (float t = 0.; t < 1.; t += .01) {
+        int x = x0 + (x1 - x0) * t;
+        int y = y0 + (y1 - y0) * t;
+        image.set(x, y, color);
+    }
+}
+
+void line_attempt_2(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+{
+    for (int x = x0; x <= x1; x++) {
+        float t = (x - x0) / (float)(x1 - x0);
+        int y = y0 * (1. - t) + y1 * t;
+        image.set(x, y, color);
+    }
+}
+
+void line_attempt_3(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+{
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {  // if the line is steep, we transpose the image
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1) {  // make it left−to−right
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    for (int x = x0; x <= x1; x++) {
+        float t = (x - x0) / (float)(x1 - x0);
+        int y = y0 * (1. - t) + y1 * t;
+        if (steep) {
+            image.set(y, x, color);  // if transposed, de−transpose
+        } else {
+            image.set(x, y, color);
+        }
+    }
+}
+void line_attempt_4(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+{
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    float derror = std::abs(dy / float(dx));
+    float error = 0;
+    int y = y0;
+    for (int x = x0; x <= x1; x++) {
+        if (steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
+        error += derror;
+        if (error > .5) {
+            y += (y1 > y0 ? 1 : -1);
+            error -= 1.;
+        }
+    }
+}
+
+void line_attempt_5(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+{
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int derror2 = std::abs(dy) * 2;
+    int error2 = 0;
+    int y = y0;
+    for (int x = x0; x <= x1; x++) {
+        if (steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
+        error2 += derror2;
+        if (error2 > dx) {
+            y += (y1 > y0 ? 1 : -1);
+            error2 -= dx * 2;
+        }
+    }
+}
+int main(int argc, char **argv)
+{
+    TGAImage image(300, 300, TGAImage::RGB);
+
+    line_attempt_5(-100, -100, 500, 500, image, white);
+    line_attempt_5(40, 40, 60, 260, image, white);
+    line_attempt_5(40, 40, 260, 60, image, white);
+
+    image.flip_vertically();  // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
+    return 0;
+}
---
+++ lesson-1/wireframe.cpp
@@ -0,0 +1,58 @@
+#include "tgaimage.h"
+#include "model.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+const int width = 800;
+const int height = 800;
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+{
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int derror2 = std::abs(dy) * 2;
+    int error2 = 0;
+    int y = y0;
+    for (int x = x0; x <= x1; x++) {
+        if (steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
+        error2 += derror2;
+        if (error2 > dx) {
+            y += (y1 > y0 ? 1 : -1);
+            error2 -= dx * 2;
+        }
+    }
+}
+int main(int argc, char **argv)
+{
+    Model model{"obj/african_head.obj"};
+    TGAImage image(width, height, TGAImage::RGB);
+    for (int i = 0; i < model.nfaces(); i++) {
+        std::vector<int> face = model.face(i);
+        for (int j = 0; j < 3; j++) {
+            Vec3f v0 = model.vert(face[j]);
+            Vec3f v1 = model.vert(face[(j + 1) % 3]);
+            int x0 = (v0.x + 1.) * width / 2.;
+            int y0 = (v0.y + 1.) * height / 2.;
+            int x1 = (v1.x + 1.) * width / 2.;
+            int y1 = (v1.y + 1.) * height / 2.;
+            line(x0, y0, x1, y1, image, white);
+        }
+    }
+
+    image.write_tga_file("output.tga");
+    return 0;
+}
